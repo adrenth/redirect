0360894be99b9eae5608226e1fdf2e30f8313c8d
Alwin Drenth <alwin@vdlp.nl>
2016-04-27 10:31:19 +0200

    Add more test for RedirectManager

--- tests/RedirectManagerTest.php
+++ tests/RedirectManagerTest.php
@@ -10,18 +10,59 @@ use PluginTestCase;
 
 class RedirectManagerTest extends PluginTestCase
 {
+    public function testExactRedirect()
+    {
+        $redirect = new Redirect([
+            'match_type' => Redirect::TYPE_EXACT,
+            'target_type' => Redirect::TARGET_TYPE_PATH_URL,
+            'from_url' => '/this-should-be-source',
+            'to_url' => '/this-should-be-target',
+            'requirements' => null,
+            'status_code' => 302,
+            'is_enabled' => 1,
+            'publish_status' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        $rule = RedirectRule::createWithModel($redirect);
+        self::assertInstanceOf(RedirectRule::class, $rule);
+
+        $manager = RedirectManager::createWithRule($rule);
+        self::assertInstanceOf(RedirectManager::class, $manager);
+
+        $test = '/this-should-be-source';
+        $result = $manager->match($test);
+
+        self::assertInstanceOf(RedirectRule::class, $result);
+        self::assertEquals('/this-should-be-target', $manager->getLocation($result));
+
+        $test = '/this-is-something-totally-different';
+        self::assertEquals(false, $manager->match($test));
+    }
+
     public function testPlaceholderRedirect()
     {
         $redirect = new Redirect([
-            'match_type' => 'placeholders',
-            'target_type' => 'path_or_url',
+            'match_type' => Redirect::TYPE_PLACEHOLDERS,
+            'target_type' => Redirect::TARGET_TYPE_PATH_URL,
             'from_url' => '/blog.php?cat={category}&section={section}&id={id}',
             'to_url' => '/blog/{category}/{section}/{id}',
             'requirements' => [
                 [
-                    'placeholder' => '',
+                    'placeholder' => '{category}',
                     'requirement' => '(octobercms|wordpress|drupal)',
-                    'replacement' => '',
+                    'replacement' => null,
+                ],
+                [
+                    'placeholder' => '{section}',
+                    'requirement' => '[a-z]+',
+                    'replacement' => null,
+                ],
+                [
+                    'placeholder' => '{id}',
+                    'requirement' => '[0-9]{2}',
+                    'replacement' => null,
                 ]
             ],
             'status_code' => 301,
@@ -32,8 +73,65 @@ class RedirectManagerTest extends PluginTestCase
         ]);
 
         $rule = RedirectRule::createWithModel($redirect);
+        self::assertInstanceOf(RedirectRule::class, $rule);
 
         $manager = RedirectManager::createWithRule($rule);
+        self::assertInstanceOf(RedirectManager::class, $manager);
 
+        $test = '/blog.php?cat=octobercms&section=test&id=1337';
+        self::assertFalse($manager->match($test));
+
+        $test = '/blog.php?cat=octobercms&section=test&id=13';
+        $result = $manager->match($test);
+        self::assertInstanceOf(RedirectRule::class, $result);
+        self::assertEquals('/blog/octobercms/test/13', $manager->getLocation($result));
+
+        $test = '/blog.php?cat=wordpress&section=test&id=99';
+        $result = $manager->match($test);
+        self::assertInstanceOf(RedirectRule::class, $result);
+        self::assertEquals('/blog/wordpress/test/99', $manager->getLocation($result));
+
+        $test = '/blog.php?cat=joomla&section=test&id=99';
+        self::assertFalse($manager->match($test));
+
+        $test = '/blog.php?cat=drupal&section=test&id=e9';
+        self::assertFalse($manager->match($test));
+    }
+
+    public function testTargetCmsPageRedirect()
+    {
+        // @TODO
+    }
+
+    public function testScheduledRedirect()
+    {
+        $redirect = new Redirect([
+            'match_type' => Redirect::TYPE_EXACT,
+            'target_type' => Redirect::TARGET_TYPE_PATH_URL,
+            'from_url' => '/this-should-be-source',
+            'to_url' => '/this-should-be-target',
+            'requirements' => null,
+            'status_code' => 302,
+            'is_enabled' => 1,
+            'publish_status' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'from_date' => Carbon::now(),
+            'to_date' => Carbon::now()->addWeek(),
+        ]);
+
+        $rule = RedirectRule::createWithModel($redirect);
+        self::assertInstanceOf(RedirectRule::class, $rule);
+
+        $manager = RedirectManager::createWithRule($rule);
+        self::assertInstanceOf(RedirectManager::class, $manager);
+
+        $manager->setMatchDate(Carbon::now()->addDay(2));
+
+        self::assertInstanceOf(RedirectRule::class, $manager->match('/this-should-be-source'));
+
+        $manager->setMatchDate(Carbon::now()->addWeek()->addDay());
+
+        self::assertFalse($manager->match('/this-should-be-source'));
     }
 }
