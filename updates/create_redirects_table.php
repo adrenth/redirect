fd7df15832c9dd2c944db7241b87cf502666041a
CSNWEB <CSNWEB@users.noreply.github.com>
2016-09-28 11:20:29 +0200

    use string instead of char

--- updates/create_redirects_table.php
+++ updates/create_redirects_table.php
@@ -20,7 +20,7 @@ class CreateRedirectsTable extends Migration
         Schema::create(self::TABLE, function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->char('match_type', '12');
+            $table->string('match_type', '12');
             $table->string('from_url');
             $table->string('to_url');
             $table->json('requirements')->nullable();
